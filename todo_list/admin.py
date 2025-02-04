1cef455663d3c06bf46cdb485d797d4bf11bdb35
Egorka28 <egorradikevic16064@gmail.com>
2025-02-04 17:08:35 +0700

    add admin

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from todo_list import models
+
 # Register your models here.
+admin.site.register(models.User)
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from todo_list.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'todo_list/index.html')
+    return render(request, 'todo_list/index.html', {
+        'users': User.objects.all()
+    })
