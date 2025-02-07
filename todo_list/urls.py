3c74905a6e8effaa524f2c8a6b494b69e2cbf14f
Egorka28 <egorradikevic16064@gmail.com>
2025-02-07 13:11:21 +0700

    correct small mistakes

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from todo_list import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index')
 ]
 
-urlpatterns += staticfiles_urlpatterns()
